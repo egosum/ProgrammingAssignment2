f56bcabaea40c4c4efb5b7715a17042e0bda95e7
Jesús Perera Aracil <egosum@gmail.com>
2014-12-20 00:13:41 +0100

    Assignment 2
    Assignment 2 completed in this commit.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,22 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        get <- function() x
+        
+        setinverted <- function(inverted) m <<- inverted
+        
+        getinverted <- function() m
+        
+        list(set = set, get = get,
+             setinverted = setinverted,
+             getinverted = getinverted)
 }
 
 
@@ -12,4 +27,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getinverted()
+        
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        
+        data <- x$get()
+        
+        m <- solve(data, ...)
+        
+        x$setinverted(m)
+        
+        m
 }
